3eb3869d3991f44601b6d1b10bb0aedd30d0f3b9
Colin Newell <colin.newell@gmail.com>
2021-09-18 20:06:20 +0100

    tweak permissions

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mkdir ssh-keys
 cp sshconfig ssh-keys/config
-sudo chown root ssh-keys/config
+sudo chmod 600 ssh-keys/config
 echo Leave passphrase empty.
 echo Note that athens does allow you to hook up to an ssh agent if you want to
 echo avoid using an empty passphrase private key.
@@ -9,3 +9,4 @@ ssh-keygen -f ssh-keys/id_rsa
 cat ssh-keys/id_rsa.pub
 echo Now install the public key in a github account with access to the private
 echo "repo's."
+sudo chown root.root -R ssh-keys/
